86994e18fd62ba0a14698662623f49347987714a
JeevArn <jeevya.aroun@sorbonne-nouvelle.fr>
2025-01-26 12:45:21 +0100

    chore: remove unused imports + change paths to models for mac

--- systeme/scripts/ocr_script.py
+++ systeme/scripts/ocr_script.py
@@ -1,28 +1,11 @@
 """
-Ce script prend en arguments un chemin vers une image de texte et son type (mot, ligne ou paragraphe) et retourne le contenu textuel de l'image.
+Ce script prend en arguments chemin vers une image de texte et son type (mot, ligne ou paragraphe) et retourne le contenu textuel de l'image.
 """
-
-
-
-import os
-import numpy as np
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torchvision import transforms
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
-from PIL import Image, ImageFont, ImageDraw
-from PIL import Image
-import cv2
-import pytesseract
-from pytesseract import Output
-from tensorflow.keras.utils import to_categorical
-from tensorflow.keras.models import Sequential, load_model
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
-import matplotlib.pyplot as plt
 import argparse
 import sys
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+from tensorflow.keras.models import load_model
 from segmentation import word_to_chars, line_to_words, doc_to_lines
 from keras.utils import disable_interactive_logging
 disable_interactive_logging()
@@ -102,14 +85,14 @@ def predict_text(docPath, docType, model, label_encoder):
 
             # Ajouter le mot au résultat
             result += predicted_word + " "
-        
+
         # Afficher le résultat
         return result
 
 
     # Si l'image est un document
     elif docType == "doc":
-        
+
         # Segmenter le document en lignes
         lines = doc_to_lines(docPath)
 
@@ -137,7 +120,7 @@ def predict_text(docPath, docType, model, label_encoder):
 
 
 def main():
-
+    """Fonction principale"""
     # Vérification du nombre d'arguments
     if len(sys.argv) != 3:
         print("Erreur : Nombre incorrect d'arguments.")
@@ -152,10 +135,10 @@ def main():
     args = parser.parse_args()
 
     # Charger le modèle d'OCR pré-entraîné
-    model = load_model('../models/OCR_50000w_20e_1.h5')
+    model = load_model('systeme/models/OCR_50000w_10e.h5')
 
     # Formater le LabelEncoder
-    classes = np.load('../data/classes_1.npy')
+    classes = np.load('systeme/data/classes_1.npy')
     label_encoder = LabelEncoder()
     label_encoder.fit(classes)
 
